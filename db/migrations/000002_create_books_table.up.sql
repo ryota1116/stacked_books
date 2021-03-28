72ec13886403b2947fee26d6a374cf6b0549c10f
船先　亮汰 <ryota1116funa@gmail.com>
2021-03-29 01:52:32 +0900

    マイグレーションファイルを追加

---
+++ db/migrations/000002_create_books_table.up.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS books (
+    id INT AUTO_INCREMENT UNIQUE NOT NULL,
+    google_books_id VARCHAR(20) UNIQUE NOT NULL,
+    title VARCHAR (255) UNIQUE NOT NULL,
+    description VARCHAR (255),
+    image VARCHAR(255),
+    isbn_10 VARCHAR(10),
+    isbn_13 VARCHAR(13),
+    page_count INT,
+    publish_at TIMESTAMP,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+
+    PRIMARY KEY (id, google_books_id)
+)
