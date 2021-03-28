1e87673ff18c201e1a6b20127a134aaedb585876
船先　亮汰 <ryota1116funa@gmail.com>
2021-03-28 22:22:44 +0900

    handlerのテストを一部追加

--- handler/user_test.go
+++ handler/user_test.go
@@ -12,13 +12,14 @@ import (
 	"github.com/ryota1116/stacked_books/domain/model"
 )
 
+// モックを導入
 type UserUseCaseMock struct {
 }
-
+// モック型でプロダクションコードの
 func (uu *UserUseCaseMock) SignUp(user model.User) (model.User, error) {
 	return model.User{
 		Id:        1,
-		UserName:  "user",
+		UserName:  "user_name",
 		Email:     "user@example.jp",
 		Password:  "password",
 		Avatar:    "",
@@ -35,7 +36,7 @@ func (uu *UserUseCaseMock) SignIn(user model.User) (string, error) {
 func (uu *UserUseCaseMock) ShowUser(params map[string]string) model.User {
 	return model.User{
 		Id:        1,
-		UserName:  "user",
+		UserName:  "user_name",
 		Email:     "user@example.jp",
 		Password:  "password",
 		Avatar:    "",
@@ -47,21 +48,23 @@ func (uu *UserUseCaseMock) ShowUser(params map[string]string) model.User {
 
 // TODO: レスポンスボディの型・値、ステータスコードをテスト
 func TestUserHandler_SignUp(t *testing.T) {
-	uu := UserUseCaseMock{}
+	// uh := NewUserHandler(&UserUseCaseMock{}) と同義
+	uu := UserUseCaseMock{} // モック生成
 	uh := NewUserHandler(&uu)
 
 	// リクエストボディ
 	bodyReader := strings.NewReader(`{
-		UserName:  "user_name",
-		Email:     "user@example.jp",
-		Password:  "password",
+		"user_name": "user_name",
+		"email": "user@example.jp",
+		"password": "password",
 	}`)
 
+	// TODO: 書き換えてもエラーにならない
 	r := httptest.NewRequest("POST", "/signup", bodyReader)
 	w := httptest.NewRecorder()
 	uh.SignUp(w, r)
 
-	response := w.Result() //レスポンスを代入
+	response := w.Result() // uh.SignUp(w, r)は戻り値無いし、レスポンスを代入してテストする
 
 	// ステータスコードのテスト
 	if response.StatusCode != 200 {
@@ -81,10 +84,36 @@ func TestUserHandler_SignUp(t *testing.T) {
 	}
 
 	assert.Equal(t, user.Id, int64(1), `ユーザーIDが正しいこと`)
+	assert.Equal(t, user.UserName, "user_name", `ユーザー名が正しいこと`)
+	assert.Equal(t, user.Password, "password", `ユーザー名が正しいこと`)
 }
 
 func TestUserHandler_SignIn(t *testing.T) {
+	TestUserHandler_SignUp(t)
+
+	uu := UserUseCaseMock{}
+	uh := NewUserHandler(&uu)
+
+	// SignInのリクエストボディ
+	signInBodyReader := strings.NewReader(`{
+		"email": "user@example.jp",
+		"password": "password",
+	}`)
+	r := httptest.NewRequest("POST", "/signin", signInBodyReader)
+	w := httptest.NewRecorder()
+	uh.SignIn(w, r)
+
+	response := w.Result() //レスポンスを代入
+
+	// ステータスコードのテスト
+	if response.StatusCode != 200 {
+		t.Errorf(`レスポンスのステータスコードは %d でした`, response.StatusCode)
+	}
+
+	//assert.Equal(t, &response.Body, "string", `ユーザーIDが正しいこと`)
 }
 
 func TestUserHandler_ShowUser(t *testing.T) {
 }
+
+
