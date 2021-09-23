29525dfa7a6a70d0e9175339f5a0f3b11f4bbe19
船先　亮汰 <ryota1116funa@gmail.com>
2021-09-23 19:27:51 +0900

    レスポンスボディのjson文字列と期待するjson文字列を比較するテストをリファクタリング

--- handler/user_book_test.go
+++ handler/user_book_test.go
@@ -1,28 +1,22 @@
 package handler
 
 import (
-	"github.com/ryota1116/stacked_books/domain/model"
+	"github.com/ryota1116/stacked_books/domain/model/dto"
+	"github.com/ryota1116/stacked_books/tests/test_assertion"
+	"io/ioutil"
 	"net/http/httptest"
 	"strings"
 	"testing"
-	"time"
 )
+
+// テストで期待するレスポンスボディJSON文字列のファイルパス
+const expectedRegisterUserBookJson = "../tests/expected/api/userBookHandler/200_register_user_book.json"
 
 type UserBookUseCaseMock struct {}
 
-func (ubum UserBookUseCaseMock) RegisterUserBook(userId int, userBookParameter model.UserBookParameter) model.UserBookParameter {
-	return model.UserBookParameter{
-		UserBook: model.UserBook{
-			Id:        1,
-			UserId:    1,
-			BookId:    1,
-			Status:    0,
-			Memo:      "メモメモメモ",
-			CreatedAt: time.Time{},
-			UpdatedAt: time.Time{},
-		},
-		Book:     model.Book{
-			Id:             1,
+func (UserBookUseCaseMock) RegisterUserBook(int, dto.RegisterUserBookRequestParameter) dto.RegisterUserBookResponse {
+	return dto.RegisterUserBookResponse{
+		Book:     dto.Book{
 			GoogleBooksId:  "Wx1dLwEACAAJ",
 			Title:          "リーダブルコード",
 			Description:    "読んでわかるコードの重要性と方法について解説",
@@ -31,8 +25,11 @@ func (ubum UserBookUseCaseMock) RegisterUserBook(userId int, userBookParameter m
 			PageCount:      237,
 			PublishedYear:  2012,
 			PublishedMonth: 6,
-			CreatedAt:      time.Time{},
-			UpdatedAt:      time.Time{},
+			PublishedDate:  0,
+		},
+		UserBook: dto.UserBook{
+			Status: 0,
+			Memo:   "メモメモメモ",
 		},
 	}
 }
@@ -41,6 +38,7 @@ func TestBookHandlerRegisterUserBook(t *testing.T) {
 	ubu := UserBookUseCaseMock{}
 	ubh := NewUserBookHandler(ubu)
 
+	// リクエストボディを検証しているならこの記述が活きてくる気がするが、、
 	bodyReader := strings.NewReader(`{
 		"google_books_id": "Wx1dLwEACAAJ",
 		"title": "リーダブルコード",
@@ -53,14 +51,32 @@ func TestBookHandlerRegisterUserBook(t *testing.T) {
 		"published_month": 6,
 	
 		"status": 0,
-		"memo": "めもめもめも"
+		"memo": "メモメモメモ"
 	}`)
 
-	r := httptest.NewRequest(
-		"GET",
-		"/register/book",
-		bodyReader)
+	r := httptest.NewRequest("GET", "/register/book", bodyReader)
 	w := httptest.NewRecorder()
 
+	// TODO: テスト時にCurrentUserメソッドでセッションからユーザー情報を取得するのどうやるか
+	r.Header.Add("Authorization", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InVzZXJfM0BleGFtcGxlLmNvbSIsImV4cCI6MTYzMjQ5ODY2MCwiaWF0IjoxNjMyMjM5NDYwLCJwYXNzd29yZCI6IiQyYSQxMCRZU0FzTmo5UldPekZDRmpLRmdDNXJlQ0JEVVROSnIyZVh1YUdxT2RWV25RWU5EenkyNk0wZSIsInVzZXJJZCI6M30.FsyIcmFk5BVl32OVordFlF2EAIj6CaqwfUudrKU5b9Y")
+
+	// この中でUserBookUseCaseMockのRegisterUserBookが実行される
 	ubh.RegisterUserBook(w, r)
+
+	// レスポンスを代入
+	response := w.Result()
+
+	// ステータスコードのテスト
+	if response.StatusCode != 200 {
+		t.Errorf(`レスポンスのステータスコードは %d でした`, response.StatusCode)
+	}
+
+	// レスポンスボディを[]byte型に変換
+	responseBodyBytes, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	// JSON文字列の比較
+	test_assertion.CompareResponseBodyWithJsonFile(t, responseBodyBytes, expectedRegisterUserBookJson)
 }
