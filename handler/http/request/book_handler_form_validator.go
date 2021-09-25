0dc9d4c6f51b704e778aa012a9e21507ff115fa1
船先　亮汰 <ryota1116funa@gmail.com>
2021-09-26 00:23:47 +0900

    リクエストボディのバリデーションを実装

---
+++ handler/http/request/book_handler_form_validator.go
@@ -0,0 +1,18 @@
+package request
+
+import (
+	"github.com/ryota1116/stacked_books/domain/model/googleBooksApi"
+	"github.com/ryota1116/stacked_books/handler/http/response"
+)
+
+type BookHandlerFormValidator struct {
+	GoogleBooksApiRequestBody googleBooksApi.RequestParameter
+}
+
+func (bh BookHandlerFormValidator) Validate() (bool, response.ErrorResponseBody) {
+	if bh.GoogleBooksApiRequestBody.Title == "" {
+		return false, response.ErrorResponseBody{Message: "本のタイトルを入力してください"}
+	}
+
+	return true, response.ErrorResponseBody{Message: ""}
+}
