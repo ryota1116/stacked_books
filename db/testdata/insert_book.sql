3a3d3e29b5ac00babb1f58cf425106402ff5452f
船先　亮汰 <ryota1116funa@gmail.com>
2021-11-23 23:59:05 +0900

    テストデータの作成

---
+++ db/testdata/insert_book.sql
@@ -0,0 +1,5 @@
+-- Bookを1件作成するSQL
+INSERT INTO books
+(id, google_books_id, title, description, isbn_10, isbn_13, page_count, published_year, published_month)
+VALUES
+(1, '1f9lDwAAQBAJ', 'リーダブルコード', '読んでわかるコードの重要性と方法について解説', '4873115655', '9784873115658', 237, 2017, 4);
